7c500d1dc282162cba3b2a57ea1ddf6a62bcfd19
Ting3l <henning@gehebajo.de>
2021-06-22 16:32:37 +0200

    Update DrinkReminder.ps1
    Added function Refresh-Config. 
    First Config-Read with first timer-tick
    Little tweaks
    removed global:-tag where it's not needed
    some formatting

--- DrinkReminder.ps1
+++ DrinkReminder.ps1
@@ -1,5 +1,5 @@
 # Drink Reminder by Ting3l
-# Version 1.1
+# Version 1.2
 # Description:
 <#
     Reminds you to drink by playing a sound (custom) and showing a notification after a set time.
@@ -13,41 +13,23 @@ Add-Type -AssemblyName presentationCore
 [System.Reflection.Assembly]::LoadWithPartialName('WindowsFormsIntegration') | out-null
 #endregion
 
-#region VarDefinitions
-$BasePath = Split-Path $MyInvocation.MyCommand.Path # Get Basepath
-
-$Warning = "$BasePath\warn.mp3" # "Warning"-sound (first one played)
-$Critical = "$BasePath\crit.mp3" # "Critical"-sound (played after some warnings)
-
-$mediaPlayer = New-Object system.windows.media.mediaplayer
-
-# Config
-$config = Get-Content -Path "$BasePath\config.txt" | ConvertFrom-Json # Get Config-File
-$DrinkTimespan = $config.timespan
-$RepeatWarning = $config.repeatwarning
-$CriticalThreshold = $config.criticalthreshold
-$HideWindow = [bool]$config.hidewindow
-$debug = [bool]$config.debug
-
-if ($debug){
-    $DrinkTimespan = 1
-    $RepeatWarning = 1
-    $CriticalThreshold = 2
-    $HideWindow = $false
-}
-
-# Icon for Taskbar
-$iconfile = Get-ChildItem "$BasePath\icon.*"
-if ($iconfile.Count -gt 1){$iconfile = $iconfile[0]}
-if ($iconfile.Name.EndsWith(".ico")){$Icon = "$($iconfile.FullName)"}
-elseif ($iconfile.Name.EndsWith(".exe")){$Icon = [System.Drawing.Icon]::ExtractAssociatedIcon("$($iconfile.FullName)")}
-
-$global:Timer = Get-Date
-$global:LastWarn = $global:Timer
-$global:WarnCount = 0
-#endregion
-
 #region CustomFunctions
+function Refresh-Config ($filepath){
+    Write-Host Refreshing configuration...
+
+    $config = Get-Content -Path $filepath | ConvertFrom-Json # Get Config-File
+    $global:DrinkTimespan = $config.timespan
+    $global:RepeatWarning = $config.repeatwarning
+    $global:CriticalThreshold = $config.criticalthreshold
+    $global:debug = [bool]$config.debug
+
+    $global:HideWindow = $true
+    if ($global:debug){$global:HideWindow = $false}
+    
+    $global:LastConfigChange = (Get-Item $filepath).LastWriteTime
+
+    Write-host $config
+}
 function Play ($Path, $Length, $Volume){
     $mediaPlayer.open($Path)
     $mediaPlayer.Volume = $Volume
@@ -62,6 +44,27 @@ function Hide-Window(){
 }
 #endregion
 
+#region VarDefinitions
+$BasePath = Split-Path $MyInvocation.MyCommand.Path # Get Basepath
+
+$Warning = "$BasePath\warn.mp3" # "Warning"-sound (first one played)
+$Critical = "$BasePath\crit.mp3" # "Critical"-sound (played after some warnings)
+
+$mediaPlayer = New-Object system.windows.media.mediaplayer
+
+$configfilepath = "$BasePath\config.txt"
+
+# Icon for Taskbar
+$iconfile = Get-ChildItem "$BasePath\icon.*"
+if ($iconfile.Count -gt 1){$iconfile = $iconfile[0]}
+if ($iconfile.Name.EndsWith(".ico")){$Icon = "$($iconfile.FullName)"}
+elseif ($iconfile.Name.EndsWith(".exe")){$Icon = [System.Drawing.Icon]::ExtractAssociatedIcon("$($iconfile.FullName)")}
+
+$Timer = Get-Date
+$LastWarn = $Timer
+$WarnCount = 0
+#endregion
+
 #region GUI
 $Main_Tool_Icon = New-Object System.Windows.Forms.NotifyIcon
 $Main_Tool_Icon.Text = "Fluid Intake"
@@ -69,15 +72,15 @@ $Main_Tool_Icon.Icon = $Icon
 $Main_Tool_Icon.Visible = $true
 
 $timestamp = New-Object System.Windows.Forms.MenuItem
-$timestamp.Text = $global:Timer
+$timestamp.Text = $Timer
 
 $drink = New-Object System.Windows.Forms.MenuItem
 $drink.Text = "Getrunken!"
 $drink.Add_Click({ 
-    $global:Timer = Get-Date
-    $global:LastWarn = $global:Timer
-    $timestamp.Text = $global:Timer
-    $global:WarnCount = 0
+    $Timer = Get-Date
+    $LastWarn = $Timer
+    $timestamp.Text = $Timer
+    $WarnCount = 0
     $myTimer.Stop()
     Start-Sleep -Milliseconds 100
     $myTimer.Start()
@@ -89,7 +92,6 @@ $exit.Text = "Beenden"
 $exit.add_Click({
     $myTimer.Enabled = $false
     $Main_Tool_Icon.Visible = $false
-    $window.Close()
     Stop-Process $pid
 })
 
@@ -98,31 +100,36 @@ $contextmenu.MenuItems.Add($timestamp) | Out-Null
 $contextmenu.MenuItems.Add($drink) | Out-Null
 $contextmenu.MenuItems.Add($exit) | Out-Null
 $Main_Tool_Icon.ContextMenu = $contextmenu
-
 $Main_Tool_Icon.add_MouseDown({$Main_Tool_Icon.GetType().GetMethod("ShowContextMenu",[System.Reflection.BindingFlags]::Instance -bor [System.Reflection.BindingFlags]::NonPublic).Invoke($Main_Tool_Icon,$null)})
 
 $myTimer = new-object System.Windows.Forms.Timer
 $myTimer.Interval = 60000
 $myTimer.add_tick({
+    if ($global:LastConfigChange -ne (Get-Item -Path $configfilepath).LastWriteTime){Refresh-Config $configfilepath}
+    
     $t = Get-Date
-    $td = ($t - $global:Timer)
-    Write-Host "Last drink: $(([String]$t.TimeOfDay).Split(".")[0]) - $(([String]$global:Timer.TimeOfDay).Split(".")[0]) = $td"
+    $td = ($t - $Timer)
+    Write-Host "Last drink: $(([String]$t.TimeOfDay).Split(".")[0]) - $(([String]$Timer.TimeOfDay).Split(".")[0]) = $td"
+    
     if ($td.TotalMinutes -ge $DrinkTimespan){
-        Write-host " Last warn: $(([String]$t.TimeOfDay).Split(".")[0]) - $(([String]$global:LastWarn.TimeOfDay).Split(".")[0]) = $($t - $global:LastWarn)"
-        if (($t - $global:LastWarn).TotalMinutes -ge $RepeatWarning){
+        Write-host " Last warn: $(([String]$t.TimeOfDay).Split(".")[0]) - $(([String]$LastWarn.TimeOfDay).Split(".")[0]) = $($t - $LastWarn)"
+        
+        if (($t - $LastWarn).TotalMinutes -ge $RepeatWarning){
             $text = "Last drink $($td.Hours)h, $($td.Minutes)m ago"
-            if ($global:WarnCount -lt $CriticalThreshold){
-                Write-Host "Playing warning (previous warn count: $global:WarnCount)"
+            
+            if ($WarnCount -lt $CriticalThreshold){
+                Write-Host "Playing warning (previous warn count: $WarnCount)"
                 $Main_Tool_Icon.ShowBalloonTip(7000, "Seek fluid intake.", $text, 'None')
                 Play $Warning 2200 0.3
             }
             else{
-                Write-Host "Playing critical warning (warn count: $global:WarnCount)"
+                Write-Host "Playing critical warning (warn count: $WarnCount)"
                 $Main_Tool_Icon.ShowBalloonTip(7000, "Seek fluid intake immediately!", $text, 'None')
                 Play $Critical 3000 1
             }
-            $global:LastWarn = Get-Date
-            $global:WarnCount++
+            
+            $LastWarn = Get-Date
+            $WarnCount++
         }
     }
 })
